794486b2c775919f265825857f82fd08beece25a
TCWU <hola7671@gmail.com>
2026-03-29 18:15:43 +0800

    init main

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	// "context"
+	// "fmt"
+	// "log"
+	// "os"
+
+	"fyneTest/ui"
+	// "github.com/tmc/langchaingo/llms"
+	// "github.com/tmc/langchaingo/llms/ollama"
+)
+
+func main() {
+	ui.BuildUI()
+
+	// TODO: 測試 gocv + Ollama，並連接 UI
+
+}
