b9f2791c0dbc0b6bd8416bf4b547e8ccff44ed7a
harry <harry.wu@emplustech.com>
2026-03-30 11:16:02 +0800

    update code

--- ui/app.go
+++ ui/app.go
@@ -13,6 +13,7 @@ import (
 )
 
 func BuildUI() {
+	isRunning := false
 	a := app.New()
 	w := a.NewWindow("VLM Camera")
 	w.Resize(fyne.NewSize(1000, 600))
@@ -20,9 +21,6 @@ func BuildUI() {
 	// 左側：攝影機畫面
 	canvasImg := canvas.NewImageFromImage(image.NewRGBA(image.Rect(0, 0, 640, 480)))
 	canvasImg.FillMode = canvas.ImageFillContain
-
-	// 啟動攝影機串流
-	go camera.StreamCamera(canvasImg) // 丟到背景執行（goroutine）
 
 	// 右側元件
 	promptEntry := widget.NewMultiLineEntry()
@@ -32,9 +30,16 @@ func BuildUI() {
 	outputLabel := widget.NewLabel("VLM 輸出將顯示在這裡")
 	outputLabel.Wrapping = fyne.TextWrapWord
 
-	startBtn := widget.NewButton("開始", func() {})
-	stopBtn := widget.NewButton("停止", func() {})
+	startBtn := widget.NewButton("開始", func() {
+		isRunning = true
+	})
+	stopBtn := widget.NewButton("停止", func() {
+		a.Quit()
+	})
 	btnRow := container.NewHBox(startBtn, stopBtn)
+
+	// 啟動攝影機串流
+	go camera.StreamCamera(canvasImg, promptEntry, outputLabel, &isRunning) // 丟到背景執行（goroutine）
 
 	// 右側佈局：從上到下
 	rightPanel := container.NewVBox(
